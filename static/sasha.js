548581a41f8dd44d5e64681056ef483082717436
evelon <ttop24658@gmail.com>
2024-03-31 22:24:45 +0700

    Registration

--- static/sasha.js
+++ static/sasha.js
@@ -1,14 +1,14 @@
     function klikaj(i)
 {
-    top.location.href='Registration.html'
+    top.location.href='index.html'
 }
 
         function profil(i)
 {
-    top.location.href='Registration.html'
+    top.location.href='index.html'
 }
 
         function statistica(i)
 {
-    top.location.href='Registration.html'
+    top.location.href='index.html'
 }
