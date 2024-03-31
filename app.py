aba92e79fdb90216a676115916c53739ec96bc82
0TheWorker0 <162857756+0TheWorker0@users.noreply.github.com>
2024-03-31 12:43:46 +0700

    Shop

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
+@app.route("/table")
+def table():
 	return render_template("Dmitry_Table.html")
+
+@app.route('/')
+def shop():
+	return render_template("Dmitry_Shop.html")
 
 app.run()
