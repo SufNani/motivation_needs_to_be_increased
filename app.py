76c821aeafe9a33e3fd8a6c25da7efeb1e944cc3
0TheWorker0 <162857756+0TheWorker0@users.noreply.github.com>
2024-04-09 20:51:16 +0700

    fixed app.py.

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/shop")
+@app.route("/index")
+@app.route("/main")
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return "<h1>Это тестовая страница!!!</h1>"
+
+@app.route("/shop")
+def shop():
     return render_template("Dmitry_Shop.html")
 
 @app.route("/table")
-def test():
+def table():
     return render_template("Dmitry_Table.html")
 
 app.run()
