694dfa8dfd9c6f787f2be2b0c91ea299d3c5ddc0
Maksim <wolker2015@gmail.com>
2024-04-09 20:58:32 +0700

    fixed routing in app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask,render_template
-app = flask(__name__)
-@app.route('/')
+
+app = Flask(__name__)
+
+@app.route("/index")
+@app.route("/main")
+@app.route("/")
 def index():
-    return render_template('index.html')
-@app.route('test')
+    return render_template("index.html")
+
+@app.route("/test")
 def test():
+    return "<h1>Это тестовая страница!!!</h1>"
+
+@app.route('/admin')
+def admin():
     return render_template('mark_admin.html')
-@app.route('test1')
-def test1():
+
+@app.route('/user')
+def user():
     return render_template('mark_user.html')
-@app.route('test2')
-def test2():
+
+@app.route('/login')
+def login():
     return render_template('mark_login.html')
 
