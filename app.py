78b1a4ec4111b93668fcf16af89b6410c10ff64a
Maksim <wolker2015@gmail.com>
2024-04-09 20:35:36 +0700

    added flask routing to index

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
+@app.route("/index")
+@app.route("/main")
 @app.route("/")
 def index():
-    return render_template("../templates/sasha_menu.html")
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return "<h1>Это тестовая страница!!!</h1>"
 
 app.run()
