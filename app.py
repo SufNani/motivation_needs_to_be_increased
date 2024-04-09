86df54e8b87dd50d97b62583099e6508c457b0fb
Mizuki <162857700+SamuraiMizuki@users.noreply.github.com>
2024-04-09 20:52:59 +0700

    push

--- app.py
+++ app.py
@@ -2,14 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index")
-@app.route("/main")
-@app.route("/")
+@app.route("/signup")
 def index():
-    return render_template("index.html")
-
-@app.route("/test")
-def test():
-    return "<h1>Это тестовая страница!!!</h1>"
+    return render_template("mizuki_signup.html")
 
 app.run()
