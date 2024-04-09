9913b9f192f9d6614ab35dc0d9a3827aec0e224e
Maksim <wolker2015@gmail.com>
2024-04-09 20:55:54 +0700

    added other routes

--- app.py
+++ app.py
@@ -2,8 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/signup")
+@app.route("/index")
+@app.route("/main")
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return "<h1>Это тестовая страница!!!</h1>"
+
+@app.route("/signup")
+def signup():
     return render_template("mizuki_signup.html")
 
 app.run()
