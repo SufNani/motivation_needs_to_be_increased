b3f3c47e2fd0f6b0da18614de23b2fbc94d03dab
0TheWorker0 <162857756+0TheWorker0@users.noreply.github.com>
2024-04-14 12:49:34 +0700

    ну капец ваще

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/index")
 @app.route("/main")
 @app.route("/")
 def index():
+    context = {
+        "city": "tomsk",
+        "user": [
+            {
+                "name": "mark",
+                "balance": 1488,
+                "account_age": 0,
+            }
+        ]
+
+    }
     return render_template("index.html")
 
 @app.route("/test")
@@ -18,9 +30,19 @@ def admin():
 
 @app.route('/user')
 def user():
-    return render_template('mark_user.html')
+    context = {
+        "user": {
+            "phone_number": "1111",
+            "email": "aaa@gmail.com",
+            "birthday": "01.01.1900",
+            "living_place": "Пушкина 1"
+        }
+
+    }
+    return render_template('mark_user.html', **context)
 
 @app.route('/login')
 def login():
     return render_template('mark_login.html')
 
+app.run()
