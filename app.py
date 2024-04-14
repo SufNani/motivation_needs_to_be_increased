951195e66911a0f60376f3424321c71d076074b1
Mizuki <162857700+SamuraiMizuki@users.noreply.github.com>
2024-04-14 12:49:34 +0700

    push

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 
 @app.route("/index")
 @app.route("/main")
 @app.route("/")
 def index():
+    context = {
+        "year": datetime.datetime.now().year,
+        "email": "info@rubius.com",
+        "phone_number": "+7-(3822)-9-7777-2",
+    }
     return render_template("index.html")
-
 @app.route("/test")
 def test():
     return "<h1>Это тестовая страница!!!</h1>"
