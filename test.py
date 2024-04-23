a53cba676da653ddd6a5d86f75c8249925a017d5
evelon <ttop24658@gmail.com>
2024-04-23 20:47:35 +0700

    update

--- test.py
+++ test.py
@@ -3,12 +3,16 @@ import sqlite3
 connection = sqlite3.connect('bal.db')
 cursor = connection.cursor()
 
-login = 'Sasha'
+login = 'sasha'
 balance = '1488'
 
 
 cursor.execute('INSERT INTO bal (balance, login) '
                'VALUES (?, ?)', (balance, login))
+
+
+
+
 
 cursor.execute('SELECT * FROM bal')
 user = cursor.fetchall()
