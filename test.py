7e094caa918739ab7292551e9d82b981b851382c
Sar-aboba <marksidorov2009@gmail.com>
2024-04-23 21:01:01 +0700

    aac

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
 import sqlite3
-conn = sqlite3.connect('static/sqlite.db')
+conn = sqlite3.connect('static/sql.db')
 cursor = conn.cursor()
-def populate_db(name,phone_number,birthday,email,living_place):
-    cursor.execute('INSERT INTO sample(name,phone_number,birthday,email,living_place) VALUES(?,?,?,?,?)', (10000,12,'01.01.01','aaa','aa'))
-    conn.commit()
-populate_db(100,11,'01.01.01','aaa@email.com','aa,1')
-cursor.execute('SELECT * from sample')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+#def populate_db(name,phone_number,birthday,email,living_place):
+#cursor.execute('INSERT INTO User(name,phone_number,birthday,email,living_place) '
+ #        'VALUES(?,?,?,?,?)', (10000,12,'01.01.01','aaa','aa'))
+#conn.commit()
+#populate_db('a',11,'01.01.01','aaa@email.com','aa,1')
+login = input('login')
+password = input('password')
+try:
+    cursor.execute('SELECT * from User where login=? and password =?', (login,password))
+    User = cursor.fetchall()
+    if User:
+      print(f'вы успешно вошли {login}')
+      print(f'ваша дата рождения{User}')
+
+except:
+    print(User)
+#cursor.execute('SELECT * from User')
+#rows = cursor.fetchall()
+#print(rows)
