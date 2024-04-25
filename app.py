a284633c8a567330fc943b6277bb69dfccf43dc6
CWorkProfile <workprofile001a@gmail.com>
2024-04-25 17:12:56 +0700

    Added SQL Database to Dmitry_Table.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -20,5 +21,19 @@ def index():
 @app.route("/test")
 def test():
     return "<h1>Это тестовая страница!!!</h1>"
+
+@app.route("/table")
+def table():
+    conn = sqlite3.connect('static/db/dmitry_table.db')
+    cur = conn.cursor()
+    context = {
+        "adepts": [
+            {
+                "name": cur.execute('SELECT name FROM adepts').fetchall()[0][i],
+                "points": cur.execute('SELECT points FROM adepts').fetchall()[0][i]
+                } for i in range(cur.execute('SELECT COUNT(*) FROM adepts').fetchall()[0][0])
+            ]
+        }
+    return render_template('Dmitry_Table.html', **context)
 
 app.run()
