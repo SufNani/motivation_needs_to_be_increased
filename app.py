9b605aa94a305fe56ccd58337278d8f968020c91
evelon <ttop24658@gmail.com>
2024-04-28 17:56:08 +0700

    update

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 def index():
     conn = sqlite3.connect('ITOG.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT name, balance FROM user WHERE id = 1')
+    cursor.execute('SELECT name, balance, birthday FROM user WHERE id = 1')
     row = cursor.fetchall()[0]
     conn.close()
     context = {
         "user": {
             "balance": row[1],
-            "name": row[0]
+            "name": row[0],
+            "birthday": row[2]
         }
     }
     return render_template("sasha_menu.html", **context)
