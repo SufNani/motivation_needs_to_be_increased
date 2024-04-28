8b4ee263358c2095e3280b86c02805508260f2c0
0TheWorker0 <162857756+0TheWorker0@users.noreply.github.com>
2024-04-28 12:45:07 +0700

    ну капец ващеs

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
 from flask import Flask,render_template
 app = Flask(__name__)
+import sqlite3
 @app.route('/')
 def index():
+    conn = sqlite3.connect('static/ITOG.db')
+    cursor = conn.cursor()
+    cursor.execute('select * from user')
+    row = cursor.fetchall()[0]
+
+    conn.close
     return render_template('index.html')
 @app.route('/admin')
 def admin():
     return render_template('mark_admin.html')
 @app.route('/user')
 def user():
-    return render_template('mark_user.html')
+    conn = sqlite3.connect('static/ITOG.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name, phone_number,email,birthday,living_place FROM user ")
+    row = cursor.fetchall()[0]
+    conn.close()
+    context = {
+        "user": {
+            "phone_number": row[1],
+            "email": row[2],
+            "birthday": row[3],
+            "living_place": row[4]
+        }
+    }
+    return render_template('mark_user.html', **context)
 @app.route('/login')
 def login():
     return render_template('mark_login.html')
+app.run()
 
