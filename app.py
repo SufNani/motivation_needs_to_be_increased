86be8cb557270dd33e57478e3a5e7a25f50d742c
Maksim <wolker2015@gmail.com>
2024-05-07 19:37:16 +0700

    added web-requesting

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import datetime
-
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 
 @app.route("/index")
@@ -16,12 +15,30 @@ def index():
         "phone_number": "+7-(3822)-9-7777-2",
     }
     return render_template("index.html")
+
+
 @app.route("/test")
 def test():
     return "<h1>Это тестовая страница!!!</h1>"
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    return render_template("mizuki_signup.html")
+    rules_has_error = False
+    print(request.method)
+    if request.method == "POST":
+        rules = request.form.get("rules")
+        password = request.form.get("password")
+        r_password = request.form.get("repeated_password")
+        email = request.form.get("email")
+        name = request.form.get("name")
+        if rules != "on":
+            rules_has_error = True
+        else:
+            conn = sqlite3.connect("ITOG.db")
+            cur = conn.cursor()
+            cur.execute("INSERT INTO user(login, email, password) VALUES (?, ?, ?)", (name, email, password))
+            return render_template("mark_user.html")
+    print(rules_has_error)
+    return render_template("mizuki_signup.html", rules_has_error=rules_has_error)
 
 app.run()
