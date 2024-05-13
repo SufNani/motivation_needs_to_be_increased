b35424b2252caaa55aecd88d9378e0d6fa39068d
base-user <base-user@my-pc>
2024-05-13 20:13:39 +0700

    Added new functions to database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ class Database:
         self.__datafile_name = database_file
         self.__data_connect = sqlite3.connect(database_file)
         self.__data_cursor = self.__data_connect.cursor()
+        self.execute = self.__data_cursor.execute
 
     def __repr__(self):
         return f'<Database ({self.__datafile_name})>'
@@ -13,10 +14,25 @@ class Database:
     def kill(self):
         self.__data_connect.commit()
         self.__data_connect.close()
-    
-    def select(columns: str, table_name: str, uslovie: str = None) -> list:
-        '''execute command "SELECT [quaries] FROM table_name WHERE [uslovie];" if uslovie =/= None or "SELECT [quaries] FROM table_name;" if uslovie = None and return result.'''
-        quire = f'SELECT {quaries} FROM {table_name}' + (f' WHERE {uslovie}' if uslovie else '') + ';'
-        data = self.__data_cursor.execute(quire.replace('"', '').replace("'", "").replace('--', '')).fetchall()
-        if len(data) == 1: return data[0]
-        else: return data
+
+    def get_columns(self, table_name: str) -> list:
+        return [e[0] for e in self.__data_cursor.execute(f'SELECT * FROM {table_name}'.replace('"', '').replace("'", "").replace('--', '')).description]
+
+    def select(self, columns: str, table_name: str, uslovie: str = None, JSON: bool = True):
+        '''execute command "SELECT [quaries] FROM table_name WHERE [uslovie];" if uslovie =/= None or "SELECT [quaries] FROM table_name;" if uslovie = None and return result if json-format if JSON option is set True, else list.'''
+        quire = f'SELECT {columns} FROM {table_name}' + (f' WHERE {uslovie}' if uslovie else '') + ';'
+        data = self.execute(quire.replace('--', '')).fetchall()
+        if not JSON:
+            if len(data) == 1: return data[0]
+            else: return data
+        rdata = {}
+        columns = columns.split(', ')
+        for elm in data:
+            ifReads = 0
+            for i in range(len(elm)):
+                if not ifReads:
+                    key = elm[i]
+                    rdata[key] = {}
+                    ifReads = 1
+                rdata[key][columns[i]] = elm[i]
+        return rdata
