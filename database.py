0aaae7b55f891cc327e8e6595e372a18f6bffdc7
CTheWorker <englishteacher@rubius.com>
2024-05-19 12:26:24 +0700

    Updated database.py

--- database.py
+++ database.py
@@ -18,9 +18,9 @@ class Database:
     def get_columns(self, table_name: str) -> list:
         return [e[0] for e in self.__data_cursor.execute(f'SELECT * FROM {table_name}'.replace('"', '').replace("'", "").replace('--', '')).description]
 
-    def select(self, columns: str, table_name: str, uslovie: str = None, JSON: bool = True):
+    def select(self, columns: str, table_name: str, groupby_column: str = None, uslovie: str = None, JSON: bool = True):
         '''execute command "SELECT [quaries] FROM table_name WHERE [uslovie];" if uslovie =/= None or "SELECT [quaries] FROM table_name;" if uslovie = None and return result if json-format if JSON option is set True, else list.'''
-        quire = f'SELECT {columns} FROM {table_name}' + (f' WHERE {uslovie}' if uslovie else '') + ';'
+        quire = f'SELECT ' + ((groupby_column + ' ') if groupby_column else '') + f'{columns} FROM {table_name}' + (f' WHERE {uslovie}' if uslovie else '') + ';'
         data = self.execute(quire.replace('--', '')).fetchall()
         if not JSON:
             if len(data) == 1: return data[0]
@@ -28,11 +28,7 @@ class Database:
         rdata = {}
         columns = columns.split(', ')
         for elm in data:
-            ifReads = 0
+            rdata[elm[0]] = {}
             for i in range(len(elm)):
-                if not ifReads:
-                    key = elm[i]
-                    rdata[key] = {}
-                    ifReads = 1
-                rdata[key][columns[i]] = elm[i]
+                rdata[elm[0]][columns[i]] = elm[i]
         return rdata
