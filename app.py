d5b2ac1d85a3a9b8b9646bb503f3c4409791fa58
Maksim <wolker2015@gmail.com>
2024-05-21 20:41:48 +0700

    fixed auth

--- app.py
+++ app.py
@@ -77,6 +77,27 @@ def login():
     return render_template('mark_login.html', error=error)
 
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    rules_has_error = False
+    print(request.method)
+    if request.method == "POST":
+        rules = request.form.get("rules")
+        password = request.form.get("password")
+        r_password = request.form.get("repeated_password")
+        email = request.form.get("email")
+        name = request.form.get("name")
+        if rules != "on":
+            rules_has_error = True
+        else:
+            conn = sqlite3.connect("database.db")
+            cur = conn.cursor()
+            cur.execute("INSERT INTO user(login, email, password) VALUES (?, ?, ?)", (name, email, password))
+            return render_template("mark_user.html")
+    print(rules_has_error)
+    return render_template("mizuki_signup.html", rules_has_error=rules_has_error)
+
+
 @app.route("/logout")
 def logout():
     session.pop('user_id', None)
