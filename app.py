856ce1a4cc9cd399909897894e7b6d9ca2d1f92f
0TheWorker0 <gulilulid@gmail.com>
2024-05-26 12:41:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import from flask import Flask, render_template, request,  redirect, url_for, datetime, sqlite3
+from flask import Flask, render_template, request,  redirect, url_for, session
+
+import datetime, sqlite3
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Replace with a secure secret key
@@ -120,36 +122,6 @@ def logout():
     session.pop('user_name', None)
     return redirect(url_for('index'))
 
-@app.route('/user')
-def user():
-    user_id = request.args.get('id')
-    password = request.args.get('password')
-    conn = database.Database('database.db')
-    context = {"user": conn.select("name, phone_number, email,birthday, living_place", "user")}
-    conn.kill()
-    return render_template('mark_user.html', **context)
-
-
-@app.route('/login',methods = ['GET', 'POST'])
-def login():
-    error = None
-    if request.method == "POST":
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        email = request.form.get('email')
-        password = request.form.get('password')
-        cursor.execute("SELECT name, phone_number,email, birthday, living_place, password FROM user where email = ?",(email,))
-        user = cursor.fetchone()
-        conn.close()
-
-        if user is None:
-            return '<h1>юзер с такой почтой не найден</h1>'
-        elif user[5] != request.form.get('pass'):
-            return "нет"
-        else:
-            return "да"
-    else:
-        return render_template('mark_login.html', error=error)
 
 @app.route("/table")
 def table():
