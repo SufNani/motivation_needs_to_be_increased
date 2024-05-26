aec2f9d76f44913c8cbc92b385f215761d945555
evelon <ttop24658@gmail.com>
2024-05-26 12:38:39 +0700

    update

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def index():
                 "birthday": row[2]
             }
     context = {"user": user}
-
     return render_template("index.html", **context)
 
 
@@ -89,6 +88,10 @@ def login():
     return render_template('mark_login.html', error=error)
 
 
+
+
+
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     rules_has_error = False
@@ -109,42 +112,13 @@ def signup():
     print(rules_has_error)
     return render_template("mizuki_signup.html", rules_has_error=rules_has_error)
 
+
 @app.route("/logout")
 def logout():
     session.pop('user_id', None)
     session.pop('user_name', None)
     return redirect(url_for('index'))
 
-@app.route('/user')
-def user():
-    user_id = request.args.get('id')
-    password = request.args.get('password')
-    conn = database.Database('database.db')
-    context = {"user": conn.select("name, phone_number, email,birthday, living_place", "user")}
-    conn.kill()
-    return render_template('mark_user.html', **context)
-
-
-@app.route('/login',methods = ['GET', 'POST'])
-def login():
-    error = None
-    if request.method == "POST":
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        email = request.form.get('email')
-        password = request.form.get('password')
-        cursor.execute("SELECT name, phone_number,email, birthday, living_place, password FROM user where email = ?",(email,))
-        user = cursor.fetchone()
-        conn.close()
-
-        if user is None:
-            return '<h1>юзер с такой почтой не найден</h1>'
-        elif user[5] != request.form.get('pass'):
-            return "нет"
-        else:
-            return "да"
-    else:
-        return render_template('mark_login.html', error=error)
 
 @app.route("/table")
 def table():
